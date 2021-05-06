5c43f0fd4810c8815f8f9ac07d88113677e13574
Ketaki Gangadhar <71887363+Ketaki-Gangadhar@users.noreply.github.com>
2021-05-06 11:11:52 +0530

    sorting

--- Sorting Algorithms/Insertion Sort.cpp
+++ Sorting Algorithms/Insertion Sort.cpp
@@ -1,6 +1,6 @@
 void insert(int arr[], int n)
 {
-    for(int x=1; x<=n; x++)
+    for(int x=1; x<n; x++)
     {
         int key=arr[x];
         int y=x-1;
@@ -9,7 +9,6 @@ void insert(int arr[], int n)
             arr[y+1]=arr[y];
             y--;
         }
-        arr[y+1]=key;
-      
+        arr[y+1]=key;    
     }
 }
