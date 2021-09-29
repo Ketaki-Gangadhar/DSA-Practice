0b70dacc3bb315bbe2e1a1e83c01f4767482a041
Ketaki Gangadhar <71887363+Ketaki-Gangadhar@users.noreply.github.com>
2021-09-29 22:33:33 +0530

    Add files via upload

---
+++ Sorting Algorithms/Counting sort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//counting sort
+void countingSort(int arr[], int n)
+{
+    //finding maximum element
+    int max_ele = INT_MIN;
+    for(int x=0; x<n; x++)
+    {
+        max_ele=max(max_ele,arr[x]);
+    }
+
+    //making temporary array of size max_ele+1
+    int temp_arr[max_ele+1] = {0};
+
+    for(int x=0; x<n; x++)
+        temp_arr[arr[x]]++;
+
+    //iterating through temp_arr and making changes in original array
+    int x=0;int i=0;
+    while(x<n)
+    {
+        if(temp_arr[x]==0)
+            x++;
+        else{
+            arr[i]=x;
+            temp_arr[x]--;
+            i++;
+        }
+    }
+}
+int main()
+{
+    int arr[] = {3,4,5,2,1,6,7,0};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    countingSort(arr,n);
+    for(int x=0; x<n; x++)
+        cout<<arr[x]<<" ";
+    return 0;
+}
